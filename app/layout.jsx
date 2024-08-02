832ad8f6d56cd23f9280bef5731c3c8f8a8771d5
Valiantsin Kasabrukhau <vkasabrukhau@gmail.com>
2024-08-02 07:31:44 -0400

    First commit, already have implemented part of the home page and some components.

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "@styles/global.css";
+
+import Nav from "@components/Nav";
+import Provide from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
