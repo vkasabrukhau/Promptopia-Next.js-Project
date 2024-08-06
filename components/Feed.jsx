b234e7984c10f533803d3fd8f80fcb488c0de3d1
Valiantsin Kasabrukhau <vkasabrukhau@gmail.com>
2024-08-06 14:25:17 -0400

    Not bug tested, is now able to add prompts and display them on the home page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
-const Feed = () => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div>Feed</div>
-  )
-}
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          prompt={post.prompt}
+          tag={post.tag}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handlSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
-export default Feed
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form action="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
