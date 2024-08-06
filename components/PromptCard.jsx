cdfcc3d2cd673190f3d5cd56f6ba04909ad9973a
Valiantsin Kasabrukhau <vkasabrukhau@gmail.com>
2024-08-06 14:28:20 -0400

    Bug fixed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,13 @@ import { usePathname, userRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }, 3000);
+  };
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -26,7 +33,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             {post.creator.email}
           </p>
         </div>
-        <div className="copy_btn" onClick={() => {}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
